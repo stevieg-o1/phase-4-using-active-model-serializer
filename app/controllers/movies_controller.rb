5a7d0688b22a4b2f5a5a9eabfe330739a9949aa0
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-27 15:18:19 +0300

    attempt one

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,24 @@
 class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  
   def index
     movies = Movie.all
     render json: movies
   end
-
+  
   def show
     movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
@@ -16,4 +26,5 @@ class MoviesController < ApplicationController
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
+
 end
